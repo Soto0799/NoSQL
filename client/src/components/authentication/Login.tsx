f8be7551cb6d079916f1bf464ecf7030d8bf8aa6
Valeria Arias Castro <107090508+Pinguinita1102@users.noreply.github.com>
2024-08-05 22:45:17 -0600

    Revert "Merge branch 'main' into Valeria_Branch"
    This reverts commit f5c937d12a63ed6dca50c491bd6e9585c2b00d02, reversing
    changes made to 64287ddf31399f46271d533adb575eb02d421961.

--- client/src/components/authentication/Login.tsx
+++ client/src/components/authentication/Login.tsx
@@ -1,17 +1,26 @@
-import { ChangeEvent, ReactNode, useState, useRef, MouseEvent } from 'react';
-
+import { ChangeEvent, ReactNode, useState, useRef, MouseEvent, useEffect } from 'react';
+import { getToken } from '../../util/storage';
 import styles from './Login.module.css';
 
-const Login = ({ onLogin, ...props }): ReactNode => {
+const Login = ({ ...props }): ReactNode => {
     const password = useRef<HTMLInputElement | null>(null);
     const [username, setUsername] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    let token: string | null = getToken();
+
+    useEffect(() => {
+        const hasToken = token !== null;
+        console.log('hasToken=', hasToken);
+        setIsLoggedIn(hasToken);
+    }, [token]);
 
     const onUsernameHandler = (e: ChangeEvent): void => {
         const input: HTMLInputElement = e.target as HTMLInputElement;
         setUsername(input.value);
     };
 
-    const onClickHandler = async (e: MouseEvent): Promise<void> => {
+    const onClickHandler = async (e: MouseEvent): void => {
         const passwordInput: HTMLInputElement =
             password.current as HTMLInputElement;
         const passwordValue: string = passwordInput.value;
@@ -40,42 +49,47 @@ const Login = ({ onLogin, ...props }): ReactNode => {
             return;
         }
 
-        const token = data.data.token;
+        token = data.data.token;
         if (token) {
-            onLogin(token);
+            localStorage.setItem("token", token as string);
+            setIsLoggedIn(true);
         }
+
     };
+
     return (
         <>
-            <div {...props} className={styles.loginContainer}>
-                <label htmlFor="username">{username}</label>
-                <div className={styles.loginBox}>
-                    <h2 className={styles.loginTitle}>Iniciar sesión</h2>
-                    <p className={styles.loginSubtitle}>
-                        ¿Es tu primera vez? <a href="/register" className={styles.registerLink}>Regístrate</a>
-                    </p>
-                    <input type="text"
-                        placeholder="User Name"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={onUsernameHandler}
-                        required
-                    />
+            {!isLoggedIn && (
+                <div {...props} className={styles.loginContainer}>
+                    <label htmlFor="username">{username}</label>
+                    <div className={styles.loginBox}>
+                        <h2 className={styles.loginTitle}>Iniciar sesión</h2>
+                        <p className={styles.loginSubtitle}>
+                            ¿Es tu primera vez? <a href="/register" className={styles.registerLink}>Regístrate</a>
+                        </p>
+                        <input type="text"
+                            placeholder="User Name"
+                            id="username"
+                            name="username"
+                            value={username}
+                            onChange={onUsernameHandler}
+                            required
+                        />
 
-                    <input type="password"
-                        placeholder="Password"
-                        id="password"
-                        name="password"
-                        ref={password}
-                        required
-                    />
+                        <input type="password"
+                            placeholder="Password"
+                            id="password"
+                            name="password"
+                            ref={password}
+                            required
+                        />
 
-                    <button type="button" onClick={onClickHandler}>
-                        Login
-                    </button>
+                        <button type="button" onClick={onClickHandler}>
+                            Login
+                        </button>
+                    </div>
                 </div>
-            </div>
+            )}
         </>
     );
 
--- server/src/controllers/usuariosController.js
+++ server/src/controllers/usuariosController.js
@@ -1,72 +1,71 @@
-
 import { fetchUsuario, createUsuario, updateUsuario, removeUsuario } from '../repositories/usuariosRepository.js';
 import crypto from 'crypto-js';
 
-
-
+// Función para obtener todos los usuarios
 export const getUsuarios = async () => {
-    return await fetchUsuario();
+    return await fetchUsuario(); // Llama a la función para obtener los usuarios desde el repositorio
 }
 
-//Nuevo usuario
+// Función para crear un nuevo usuario
 export const postUsuario = async (usuario) => {
-
-    //vienen verificaciones de inputs llenados
-
+    // Verificar si el nombre de usuario está presente
     if (!usuario.username) {
-        throw new Error('El nombre del usuario no fue ingresado');}
-    
+        throw new Error('El nombre de usuario es requerido'); // Error si no hay nombre de usuario
+    }
 
+    // Verificar si la contraseña está presente
     if (!usuario.password) {
-        throw new Error('La contraseña no fue ingresada');}
-    
+        throw new Error('La contraseña es requerida'); // Error si no hay contraseña
+    }
 
-
-    // viene encriptación de contraseñas
+    // Hashear la contraseña antes de guardarla
     usuario.hashPassword = crypto.MD5(usuario.password).toString();
-    delete usuario.password;
+    delete usuario.password; // Eliminar la contraseña en texto plano
 
+    // Verificar si el usuario ya existe
     const existingUsuario = await fetchUsuario(usuario.username);
     if (existingUsuario) {
-        throw new Error('El usuario ingresado no esta disponible');}
-    
+        throw new Error('El nombre de usuario ya existe'); // Error si el usuario ya existe
+    }
 
-
-
+    // Crear el nuevo usuario
     const createdUsuario = await createUsuario(usuario);
     const result = {
-        'id': createdUsuario.insertedId
+        'id': createdUsuario.insertedId // Retorna el ID del usuario creado
     };
 
     return result;
 }
 
-
-
+// Función para obtener un usuario por nombre de usuario
 export const getUsuario = async (username) => {
+    return await fetchUsuario(username); // Llama a la función para obtener el usuario específico desde el repositorio
+}
 
-    return await fetchUsuario(username); }
-
-
-
+// Función para actualizar un usuario
 export const putUsuario = async (username, usuario) => {
+    // Verificar si el nombre de usuario está presente
     if (!username) {
-        throw new Error('El nombre de usuario no fue ingresado');}
-    
+        throw new Error('El nombre de usuario es requerido'); // Error si no hay nombre de usuario
+    }
+
+    // Hashear la nueva contraseña si está presente
     if (usuario.password) {
         usuario.hashPassword = crypto.MD5(usuario.password).toString();
-        delete usuario.password;}
-    
+        delete usuario.password; // Eliminar la contraseña en texto plano
+    }
 
-    const updatedUsuario = await updateUsuario(username, usuario);
-
+    const updatedUsuario = await updateUsuario(username, usuario); // Llama a la función para actualizar el usuario
     return updatedUsuario;
 }
 
+// Función para eliminar un usuario
 export const deleteUsuario = async (username) => {
+    // Verificar si el nombre de usuario está presente
     if (!username) {
-        throw new Error('El nombre de usuario es requerido');}
-    const deletedUsuario = await removeUsuario(username);
-    
+        throw new Error('El nombre de usuario es requerido'); // Error si no hay nombre de usuario
+    }
+
+    const deletedUsuario = await removeUsuario(username); // Llama a la función para eliminar el usuario
     return deletedUsuario;
 }
